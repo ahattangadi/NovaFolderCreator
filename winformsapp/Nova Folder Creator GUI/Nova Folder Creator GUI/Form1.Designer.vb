d5c277e03507d7e86d73c0639da1cde294aead8d
AaravHattangadi <69454819+AaravHattangadi@users.noreply.github.com>
2022-01-10 19:54:19 +0530

    Improved UI, changed create folders and preview buttons to be smaller ✅

--- winformsapp/Nova Folder Creator GUI/Nova Folder Creator GUI/Form1.Designer.vb
+++ winformsapp/Nova Folder Creator GUI/Nova Folder Creator GUI/Form1.Designer.vb
@@ -55,9 +55,9 @@ Partial Class Form1
         'btnSelOptDir
         '
         Me.btnSelOptDir.FlatStyle = System.Windows.Forms.FlatStyle.System
-        Me.btnSelOptDir.Location = New System.Drawing.Point(24, 94)
+        Me.btnSelOptDir.Location = New System.Drawing.Point(25, 94)
         Me.btnSelOptDir.Name = "btnSelOptDir"
-        Me.btnSelOptDir.Size = New System.Drawing.Size(486, 29)
+        Me.btnSelOptDir.Size = New System.Drawing.Size(471, 29)
         Me.btnSelOptDir.TabIndex = 1
         Me.btnSelOptDir.Text = "Select Output Directory"
         Me.btnSelOptDir.UseVisualStyleBackColor = True
@@ -87,16 +87,16 @@ Partial Class Form1
         '
         Me.Button2.AllowDrop = True
         Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.System
-        Me.Button2.Location = New System.Drawing.Point(24, 138)
+        Me.Button2.Location = New System.Drawing.Point(25, 129)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(486, 29)
+        Me.Button2.Size = New System.Drawing.Size(471, 29)
         Me.Button2.TabIndex = 7
         Me.Button2.Text = "Enter Folders"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'TextBox3
         '
-        Me.TextBox3.Location = New System.Drawing.Point(24, 203)
+        Me.TextBox3.Location = New System.Drawing.Point(25, 195)
         Me.TextBox3.Multiline = True
         Me.TextBox3.Name = "TextBox3"
         Me.TextBox3.ReadOnly = True
@@ -107,7 +107,7 @@ Partial Class Form1
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(24, 180)
+        Me.Label4.Location = New System.Drawing.Point(25, 172)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(153, 20)
         Me.Label4.TabIndex = 8
@@ -116,10 +116,10 @@ Partial Class Form1
         'Button3
         '
         Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.System
-        Me.Button3.Font = New System.Drawing.Font("Segoe UI", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Button3.Location = New System.Drawing.Point(24, 242)
+        Me.Button3.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button3.Location = New System.Drawing.Point(24, 244)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(295, 81)
+        Me.Button3.Size = New System.Drawing.Size(295, 36)
         Me.Button3.TabIndex = 10
         Me.Button3.Text = "Create Folders"
         Me.Button3.UseVisualStyleBackColor = True
@@ -131,7 +131,7 @@ Partial Class Form1
         Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ProfilesToolStripMenuItem, Me.AboutToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(522, 28)
+        Me.MenuStrip1.Size = New System.Drawing.Size(526, 28)
         Me.MenuStrip1.TabIndex = 11
         Me.MenuStrip1.Text = "MenuStrip1"
         '
@@ -162,10 +162,10 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Font = New System.Drawing.Font("Segoe UI", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Button1.Location = New System.Drawing.Point(325, 242)
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.Location = New System.Drawing.Point(325, 244)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(170, 81)
+        Me.Button1.Size = New System.Drawing.Size(171, 36)
         Me.Button1.TabIndex = 12
         Me.Button1.Text = "Preview"
         Me.Button1.UseVisualStyleBackColor = True
@@ -174,7 +174,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(522, 335)
+        Me.ClientSize = New System.Drawing.Size(526, 295)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.TextBox3)
